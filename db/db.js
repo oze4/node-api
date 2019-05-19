163f3b4c1f2db215da60cf665c847c224891eda5
Matt Oestreich <matthewpoestreich@gmail.com>
2019-05-19 01:41:24 -0500

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,11 +1,10 @@
 'use strict'
-const config = require('../utils/config.js');
-const mongoose = require('mongoose');
+const config       = require('../utils/config.js');
+const mongoose     = require('mongoose');
 const mongoBaseUrl = `${config.db.connectionString}/${config.db.authenticationDatabase}`;
+const mongoDB      = mongoose.createConnection(mongoBaseUrl, { useNewUrlParser: true });
 
 mongoose.set('useCreateIndex', true) // needed to suppress errors
-const mongoDB = mongoose.createConnection(mongoBaseUrl, { useNewUrlParser: true });
-
 
 
 module.exports = mongoDB;
